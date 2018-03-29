640ab4ee7070553633e19d124826e7309772cab3
RaumanalysenChristianMueller <raumanalysen@mailbox.org>
2018-03-29 17:58:51 +0200

    Added description

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # AddressAllocator
+The AddressAllocator geocodes addresses in a three step procedure:
+1. Matching with official administrative building references (tested for Germany - Northrhine-Westfalia)
+2. Geocoding by Nominatim/OSM API
+3. Geocoding by Google API
+This script is designed for usage in QGIS
--- allocateAddresses.R
+++ allocateAddresses.R
@@ -2,8 +2,8 @@
 # Author: Raumanalysen - Christian Mueller
 # raumanalysen@mailbox.org
 
-# This script geocodes addresses in a three step procedure:
-# 1. Matching with official administrative addresses (tested for Germany - Northrhine-Westfalia)
+# The AddressAllocator geocodes addresses in a three step procedure:
+# 1. Matching with official administrative building references (tested for Germany - Northrhine-Westfalia)
 # 2. Geocoding by Nominatim/OSM API
 # 3. Geocoding by Google API
 # This script is designed for usage in QGIS
